c288099bece419c84ff1d5a8b5b19becf5d4e1ab
mohd ilyas <ilyas.faizan@accenflair.com>
2022-06-03 20:25:20 +0530

    not finished

---
+++ Frontend/tawakkalayurvedic/src/Components/Main/Landing.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../Footer/Footer";
+import { Header } from "../Header/Header";
+import Main from "./Main";
+
+export const Landing = () => {
+  return (
+    <div>
+      <Header />
+      <br />
+      <Main />
+      <br />
+      <Footer />
+    </div>
+  );
+};
